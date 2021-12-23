7273d945d659a19c67d18d10d5f848cd15cc7000
Ioana Sechel <ioana.sechel@golfgenius.com>
2021-12-23 12:14:14 +0200

    Add golfer page

---
+++ pages/golfers/[id].js
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router'
+import Layout from '../../components/Layout'
+import ScoreCard from '../../components/ScoreCard'
+import useGetScores from '../../lib/useGetScores'
+import {USER_SCORES_URL } from '../../lib/useGetScores'
+
+const GolferScores = () => {
+  const router = useRouter()
+  const { id } = router.query
+  let { name, scores, error } = useGetScores(id)
+
+  return (
+  <Layout>
+    { error? (
+      error.info.errors
+    ) : (
+      name && (
+        <>
+          <h1> The scores of user {name}: </h1>
+          {scores && scores.map(score => (
+            <ScoreCard
+              key={score.id}
+              id={score.id}
+              totalScore={score.total_score}
+              playedAt={score.played_at}
+              userId={score.user_id}
+              userName={name}
+            />
+          ))}
+        </>
+      )
+    )}
+  </Layout>
+  )
+}
+
+export default GolferScores
